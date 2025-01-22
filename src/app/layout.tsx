70cbcd8e0047706d29727f006b6717dbd23dc1e0
jjward25 <45015765+jjward25@users.noreply.github.com>
2025-01-22 16:49:09 -0500

    try2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from 'next/font/google'
 import "./globals.css"
-import Sidebar from "@/devComponents/sidebar/Sidebar"
 
 const inter = Inter({ subsets: ["latin"] })
 
